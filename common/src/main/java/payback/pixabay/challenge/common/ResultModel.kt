2725580ef0b551ea2e65566c78cdeb1351225645
Parisa Delfani <parinexus@gmail.com>
2024-08-30 01:47:04 +0330

    Add extension and result model in common for share between modules

---
+++ common/src/main/java/payback/pixabay/challenge/common/ResultModel.kt
@@ -0,0 +1,6 @@
+package payback.pixabay.challenge.common
+
+sealed class ResultModel<T>(val data: T? = null, val message: String? = null) {
+    class Success<T>(data: T) : ResultModel<T>(data)
+    class Error<T>(message: String?, data: T? = null) : ResultModel<T>(data, message)
+}
