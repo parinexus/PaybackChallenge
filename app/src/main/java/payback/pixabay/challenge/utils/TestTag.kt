9158e5c659423573150b6ff3f975fa0e9b62e37b
Parisa Delfani <parinexus@gmail.com>
2024-08-30 01:57:28 +0330

    Add test tag for ui tests

---
+++ app/src/main/java/payback/pixabay/challenge/utils/TestTag.kt
@@ -0,0 +1,15 @@
+package payback.pixabay.challenge.utils
+
+object TestTag {
+    const val USERNAME_TAG = "username_tag"
+    const val IMAGE_COLLECTION_TAG = "image_collection_tag"
+    const val IMAGE_DETAIL_TAG = "image_detail"
+    const val DETAIL_SCREEN_TAGS = "tags_tag"
+    const val EMPTY_IMAGE_LIST_TAG = "empty_image_list_tag"
+    const val IMAGE_ITEM_TAG = "image_item_tag"
+    const val LARGE_IMAGE_TAG = "large_image_tag"
+    const val SOURCE_CREDIT_TAG = "source_credit_tag"
+    const val LIKE_TAG = "like_tag"
+    const val DOWNLOAD_TAG = "download_tag"
+    const val COMMENT_TAG = "comment_tag"
+}
