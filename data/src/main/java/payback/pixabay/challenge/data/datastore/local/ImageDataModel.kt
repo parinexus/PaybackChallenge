bf1397084492c6dd287a1084f87745b4fc4f00b6
Parisa Delfani <parinexus@gmail.com>
2024-08-30 00:59:40 +0330

    Add data store for local and remote implementation

---
+++ data/src/main/java/payback/pixabay/challenge/data/datastore/local/ImageDataModel.kt
@@ -0,0 +1,24 @@
+package payback.pixabay.challenge.data.datastore.local
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "image_details")
+data class ImageDataModel(
+    @PrimaryKey
+    val id: Long,
+    val searchQuery: String,
+    val pageURL: String,
+    val type: String,
+    val tags: String,
+    val previewURL: String,
+    val webFormatURL: String,
+    val largeImageURL: String,
+    val downloads: Long,
+    val likes: Long,
+    val comments: Long,
+    val userId: Long,
+    val user: String,
+    val userImageURL: String,
+    val createdAt: Long
+)
