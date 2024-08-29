d7fc141214a40d451f801fedd54f762aaa18c49d
Parisa Delfani <parinexus@gmail.com>
2024-08-30 01:58:58 +0330

    Add app storage class in app module for handle imageDao

---
+++ app/src/main/java/payback/pixabay/challenge/db/AppStorage.kt
@@ -0,0 +1,31 @@
+package payback.pixabay.challenge.db
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import payback.pixabay.challenge.data.datastore.local.ImageDataModel
+import payback.pixabay.challenge.data.datastore.local.ImageRepositoryDao
+
+@Database(
+    entities = [ImageDataModel::class],
+    version = AppStorage.VERSION,
+    exportSchema = false
+)
+abstract class AppStorage : RoomDatabase() {
+
+    abstract fun imageDao(): ImageRepositoryDao
+
+    companion object {
+        internal const val VERSION = 1
+        private const val NAME = "image_db"
+
+        fun create(applicationContext: Context): AppStorage {
+            return Room.databaseBuilder(
+                applicationContext,
+                AppStorage::class.java,
+                NAME
+            ).fallbackToDestructiveMigration().build()
+        }
+    }
+}
