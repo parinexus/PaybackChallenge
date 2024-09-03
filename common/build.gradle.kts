38ade79a8b746bc0eeb23686904d2fe4e6ba40a6
Parisa Delfani <parinexus@gmail.com>
2024-09-03 15:05:50 +0330

    Change common gradle to kts and fix dependencies

---
+++ common/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    id(libs.plugins.android.library.get().toString())
+    id(libs.plugins.kotlin.android.get().toString())
+}
+
+android {
+    compileSdk = libs.versions.compile.sdk.get().toInt()
+    namespace = "payback.pixabay.challenge.common"
+
+    defaultConfig {
+        minSdk = libs.versions.min.sdk.get().toInt()
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+}
