a453b00e7ddd0a15233f543684e228c27e4a01fa
Parisa Delfani <parinexus@gmail.com>
2024-09-10 16:29:21 +0330

    Add flow in return type in remote data source

--- data/src/main/java/com/pixabay/challenge/data/interfaces/RemoteImageDataSource.kt
+++ data/src/main/java/com/pixabay/challenge/data/interfaces/RemoteImageDataSource.kt
@@ -1,7 +1,8 @@
 package com.pixabay.challenge.data.interfaces
 
 import com.pixabay.challenge.domain.model.ImageDomainModel
+import kotlinx.coroutines.flow.Flow
 
 interface RemoteImageDataSource {
-    suspend fun fetchImages(query: String): List<ImageDomainModel>
+    suspend fun fetchImages(query: String): Flow<Result<List<ImageDomainModel>>>
 }
