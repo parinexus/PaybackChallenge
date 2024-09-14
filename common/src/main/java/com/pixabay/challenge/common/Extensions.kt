3a8ea141f54a62b9fdb2e3a07e306fcc08bfecfc
Parisa Delfani <parinexus@gmail.com>
2024-09-14 18:36:42 +0330

    Move android extension to app layer and remove from common

--- common/src/main/java/com/pixabay/challenge/common/Extensions.kt
+++ common/src/main/java/com/pixabay/challenge/common/Extensions.kt
@@ -1,14 +1,8 @@
 package com.pixabay.challenge.common
 
-import android.content.Context
-import android.content.Intent
-import android.net.Uri
 import java.text.DecimalFormat
 
 fun Long?.orZero() = this ?: 0
+fun Int?.orZero() = this ?: 0
 
 fun Long.format(): String = DecimalFormat("#,###.##").format(this)
-
-fun Context.openBrowser(url: String) {
-    startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(url)))
-}
