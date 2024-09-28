bc6df0538e349365dcad24eb37f872e5d6a22a2c
Parisa <parinexus@gmail.com>
2024-09-28 15:54:02 +0330

    Fix get images list using flow

--- data/src/main/java/com/pixabay/challenge/data/interfaces/LocalImageDataSource.kt
+++ data/src/main/java/com/pixabay/challenge/data/interfaces/LocalImageDataSource.kt
@@ -2,10 +2,11 @@ package com.pixabay.challenge.data.interfaces
 
 import com.pixabay.challenge.data.datastore.local.ImageLocalModel
 import com.pixabay.challenge.domain.model.ImageDomainModel
+import kotlinx.coroutines.flow.Flow
 
 interface LocalImageDataSource {
-    suspend fun fetchImagesWithTimestamp(query: String): Pair<List<ImageDomainModel>, Long>
-    suspend fun fetchImages(query: String): List<ImageDomainModel>
+    fun fetchImagesWithTimestamp(query: String): Flow<Pair<List<ImageDomainModel>?, Long>>
+    fun fetchImages(query: String): Flow<List<ImageDomainModel>>
     suspend fun removeImagesByQuery(query: String)
     suspend fun saveImages(images: List<ImageLocalModel>?)
     suspend fun getLastUpdateTime(): Long
---
+++ data/src/main/java/com/pixabay/challenge/data/repository/ImagesDataRepository.kt
@@ -0,0 +1,55 @@
+package com.pixabay.challenge.data.repository
+
+import com.pixabay.challenge.common.ResultModel
+import com.pixabay.challenge.data.interfaces.LocalImageDataSource
+import com.pixabay.challenge.data.interfaces.RemoteImageDataSource
+import com.pixabay.challenge.domain.model.ImageDomainModel
+import com.pixabay.challenge.domain.repository.ImageRepository
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.firstOrNull
+import javax.inject.Inject
+
+private const val FETCH_INTERVAL_IN_SECONDS: Int = 24 * 60 * 60
+
+class ImagesDataRepository @Inject constructor(
+    private val localImageDataSource: LocalImageDataSource,
+    private val remoteImageDataSource: RemoteImageDataSource,
+) : ImageRepository {
+
+    override suspend fun fetchImages(query: String): ResultModel<List<ImageDomainModel>> {
+        val localDataFlow = localImageDataSource.fetchImagesWithTimestamp(query)
+
+        return localDataFlow.firstOrNull()?.let { (localData, localTimestamp) ->
+            val nonNullLocalData = localData ?: emptyList()
+
+            if (nonNullLocalData.isEmpty() || isRefreshNeeded(localTimestamp)) {
+                fetchAndPersistData(query)
+            } else {
+                ResultModel.Success(nonNullLocalData)
+            }
+        } ?: fetchAndPersistData(query)
+    }
+
+    private suspend fun fetchAndPersistData(query: String): ResultModel<List<ImageDomainModel>> {
+        return try {
+            return when (val result = remoteImageDataSource.fetchImages(query).first()) {
+                is ResultModel.Success -> {
+                    localImageDataSource.saveImages(result.data)
+                    ResultModel.Success(localImageDataSource.fetchImages(query).first())
+                }
+                is ResultModel.Error -> {
+                    ResultModel.Error(message = result.message, data = null)
+                }
+            }
+        } catch (e: Exception) {
+            ResultModel.Error(e.localizedMessage ?: "Unknown error")
+        }
+    }
+
+    private fun isRefreshNeeded(lastFetchTimeInSeconds: Long): Boolean {
+        return getCurrentTimeInSeconds() - lastFetchTimeInSeconds >= FETCH_INTERVAL_IN_SECONDS
+    }
+
+    private fun getCurrentTimeInSeconds(): Long = System.currentTimeMillis() / 1000L
+}
+
