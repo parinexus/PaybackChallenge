bf56a62aa481c9bf7dbcc80f08f2f797a42bb9a1
Parisa <parinexus@gmail.com>
2024-09-28 15:54:52 +0330

    Fix get api response using Result Model and flow

--- data/src/main/java/com/pixabay/challenge/data/interfaces/RemoteImageDataSource.kt
+++ data/src/main/java/com/pixabay/challenge/data/interfaces/RemoteImageDataSource.kt
@@ -1,8 +1,11 @@
 package com.pixabay.challenge.data.interfaces
 
+import com.pixabay.challenge.common.ResultModel
+import com.pixabay.challenge.data.datastore.local.ImageLocalModel
+import com.pixabay.challenge.data.model.ImageRemoteModel
 import com.pixabay.challenge.domain.model.ImageDomainModel
 import kotlinx.coroutines.flow.Flow
 
 interface RemoteImageDataSource {
-    suspend fun fetchImages(query: String): Flow<Result<List<ImageDomainModel>>>
+    fun fetchImages(query: String): Flow<ResultModel<List<ImageLocalModel>>>
 }
