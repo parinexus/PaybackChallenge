c4fb61444016107c28b0c0b003115a6db6cab3b4
Parisa <parinexus@gmail.com>
2024-09-28 15:57:54 +0330

    Fix ImagesUseCase return list base on new changed in repository

--- domain/src/main/java/com/pixabay/challenge/domain/repository/ImageRepository.kt
+++ domain/src/main/java/com/pixabay/challenge/domain/repository/ImageRepository.kt
@@ -1,7 +1,8 @@
 package com.pixabay.challenge.domain.repository
 
+import com.pixabay.challenge.common.ResultModel
 import com.pixabay.challenge.domain.model.ImageDomainModel
 
 interface ImageRepository {
-    suspend fun fetchImages(query: String): List<ImageDomainModel>
+    suspend fun fetchImages(query: String): ResultModel<List<ImageDomainModel>>
 }
